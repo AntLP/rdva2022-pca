241a47de04bf08abbecaa2ea9eaf79be1f1175f0
Antoine Lachance-Perreault <antoine.lach@gmail.com>
2022-05-23 15:15:31 -0400

    ajouts multiples

--- src/generate_points_pca_example.R
+++ src/generate_points_pca_example.R
@@ -7,7 +7,7 @@ nsim <- 8
 
 points <- tibble(
   a = runif(nsim, 0, 5),
-  b = a * 2 / 5 + rnorm(nsim, 1, 1)
+  b = a * 2 / 5 + rnorm(nsim, 0, 0.5) + 2
 )
 
 pca_res <- points %>% 
@@ -38,17 +38,31 @@ points <- bind_cols(
     ),
   pca_res %>% 
     bake(new_data = NULL)
-)
+) %>% arrange(a_scaled)
 
 
 
 
 write_csv(points, "./data/pca_example.csv")
 
-points %>% 
+points %>%
   ggplot(aes(x = a, y = b)) +
-  geom_point()
+  geom_point() +
+  coord_cartesian(ylim = c(0, 8))
 
-pca_res$steps[[1]]$res
+points %>%
+  ggplot(aes(x = a_scaled, y = b_scaled)) +
+  geom_point() +
+  geom_hline(yintercept = 0) +
+  geom_vline(xintercept = 0) +
+  geom_hline(yintercept = 1, linetype = "dashed") +
+  geom_vline(xintercept = 1, linetype = "dashed") +
+  stat_smooth(method = "lm", se = F)
+# 
+# pca_res$steps[[1]]$res
+# 
+ prcomp(points %>% select(a, b), center = T, scale. = F)
 
-prcomp(points %>% select(a, b), center = T, scale. = T)
+
+
+
--- src/pca_viz.py
+++ src/pca_viz.py
@@ -1,9 +1,10 @@
-# TODO: Évaluer l'utilisation de VGroup au lieu de lists pour hold des mobjects
+# -s pour save la final position
 
 from manim import *
 import numpy as np
 import pandas as pd
 import random
+from operator import itemgetter
 
 PROMUTUEL_YELLOW = "#FFC300"
 PROMUTUEL_GREY = "#adafb2"
@@ -79,7 +80,7 @@ class center_points(PCA):
         points_centered = VGroup(*[Dot([row["a_centered"], row["b_centered"], 0], color = PROMUTUEL_YELLOW) for _, row in data.iterrows()])
         self.add(self.plane, points_init)
         self.play(Transform(points_init, points_centered, run_time = 2))
-        self.wait(15)
+        self.wait(3)
 
 
 class scale_points(PCA):
@@ -92,19 +93,18 @@ class scale_points(PCA):
 
         self.add(self.plane, points_centered)
         self.play(Transform(points_centered, points_scaled, run_time = 2))
-        self.wait(15)
-
+        self.wait(3)
 
 class distance_vis(PCA):
     def construct(self):
         data = self.import_points()
         points_scaled = VGroup(*[Dot([row["a_scaled"], row["b_scaled"], 0], color = PROMUTUEL_YELLOW) for _, row in data.iterrows()])
-        PC1 = Line([-10, -0, 0], [10, 0, 0])
+        PC1 = Line([-10, -0, 0], [10, 0, 0], color = BLUE)
 
         projections = self.create_projections(points_scaled, PC1)
         
-        dist_brace = Brace(Line([0, 0, 0], projections["points"][2].get_center(), color = RED), direction=DOWN)
-        dist_formula = MathTex("d = {:.2f}".format(self.calc_square_dist(projections["points"][2])**0.5)).next_to(dist_brace, DOWN, buff=0)
+        dist_brace = Brace(Line([0, 0, 0], projections["points"][7].get_center(), color = RED), direction=DOWN)
+        dist_formula = MathTex("d^2 = {:.2f}".format(self.calc_square_dist(projections["points"][7]))).next_to(dist_brace, DOWN, buff=0)
 
 
         self.initial_setup()       
@@ -114,28 +114,89 @@ class distance_vis2(PCA):
     def construct(self):
         data = self.import_points()
         points_scaled = VGroup(*[Dot([row["a_scaled"], row["b_scaled"], 0], color = PROMUTUEL_YELLOW) for _, row in data.iterrows()])
-        PC1 = Line([-10, -0, 0], [10, 0, 0])
+        PC1 = Line([-10, -0, 0], [10, 0, 0], color = BLUE)
 
         projections = self.create_projections(points_scaled, PC1)
         
-        dist_brace = BraceBetweenPoints([0, 0, 0], projections["points"][2].get_center())
-        dist_brace.add_updater(lambda d: d.become(BraceBetweenPoints([0, 0, 0], projections["points"][2].get_center())))
-        dist_formula = MathTex("d = {:.2f}".format(self.calc_square_dist(projections["points"][2])**(0.5))).next_to(dist_brace, DOWN, buff=0)
-        dist_formula.add_updater(lambda d: d.become(MathTex("d = {:.2f}".format(self.calc_square_dist(projections["points"][2])**(0.5)))).next_to(dist_brace, DOWN, buff=0))
+        dist_brace = BraceBetweenPoints([0, 0, 0], projections["points"][7].get_center())
+        dist_brace.add_updater(lambda d: d.become(BraceBetweenPoints([0, 0, 0], projections["points"][7].get_center())))
+        dist_formula = MathTex("d^2 = {:.2f}".format(self.calc_square_dist(projections["points"][7]))).next_to(dist_brace, DOWN, buff=0)
+        dist_formula.add_updater(lambda d: d.become(MathTex("d^2 = {:.2f}".format(self.calc_square_dist(projections["points"][7])))).next_to(dist_brace, DOWN, buff=0))
 
 
         self.initial_setup()       
         self.add(self.plane, points_scaled, PC1, projections, dist_brace, dist_formula)
         self.play(Rotate(PC1, angle = TAU/20, about_point = ORIGIN, run_time = 0.5))
-        self.wait(60)
+        self.wait(15)
 
 
+class tot_distance_calc(PCA):
+    def construct(self):
+        data = self.import_points()
+        points_scaled = VGroup(*[Dot([row["a_scaled"], row["b_scaled"], 0], color = PROMUTUEL_YELLOW) for _, row in data.iterrows()])
+        PC1 = Line([-10, -0, 0], [10, 0, 0], color = BLUE).rotate(TAU/20, about_point = ORIGIN)
+        projections = self.create_projections(points_scaled, PC1)
+
+        curr_point = projections["points"][7]
+
+        dist_brace = BraceBetweenPoints([0, 0, 0], curr_point.get_center())
+        # dist_brace.add_updater(lambda d: d.become(BraceBetweenPoints([0, 0, 0], curr_point.get_center())))
+        dist_formula = MathTex("d^2_8 = ").next_to(dist_brace, DOWN, buff=0).shift(0.20 * LEFT)
+        #dist_formula.add_updater(lambda d: d.become(MathTex("d^2 = ")).next_to(dist_brace, DOWN if dist_brace.get_center()[0] > 0 else UP, buff=0))
+
+        
+        dist_value = MathTex("{:.2f}".format(self.calc_square_dist(curr_point))).next_to(dist_formula, RIGHT, buff=0).shift(0.10 * RIGHT + 0.05 * DOWN)
+        #dist_value.add_updater(lambda d: d.become(MathTex("{:.2f}".format(self.calc_square_dist(curr_point)))).next_to(dist_formula, RIGHT, buff=0))
+
+
+
+        x_val = [i.get_center()[0] for i in projections["points"]]
+        projections["points"] = VGroup(*[projections["points"][i] for i in [x_val.index(i) for i in sorted(x_val)]])
+
+        cum_total = 0
+        cum_formula = MathTex(r"\sum d^2_i = ").to_corner(DR).shift(1.15 * LEFT)
+        cum_value = MathTex("{:.2f}".format(cum_total)).to_corner(DR).shift(0.20 * UP)
+
+
+        self.initial_setup()       
+        self.add(self.plane, points_scaled, PC1, projections, dist_brace, dist_formula, dist_value, cum_formula, cum_value)
+
+        anim_run_time = 0.7
+
+        for i in range(len(points_scaled) - 1, -1, -1):
+
+            
+
+            new_dist_brace = BraceBetweenPoints([0, 0, 0], projections["points"][i].get_center())
+            if projections["points"][i].get_center()[0] > 0:
+                new_dist_formula = MathTex("d^2_{} = ".format(i+1)).next_to(new_dist_brace, DOWN, buff=0).shift(0.20 * LEFT)
+                new_dist_value = MathTex("{:.2f}".format(self.calc_square_dist(projections["points"][i]))).next_to(new_dist_formula, RIGHT, buff=0).shift(0.10 * RIGHT + 0.05 * DOWN)
+            else:
+                new_dist_formula = MathTex("d^2_{} = ".format(i+1)).next_to(new_dist_brace, UP, buff=0).shift(0.20 * LEFT)
+                new_dist_value = MathTex("{:.2f}".format(self.calc_square_dist(projections["points"][i]))).next_to(new_dist_formula, RIGHT, buff=0).shift(0.10 * RIGHT + 0.05 * DOWN)
+
+            new_dist_formula2 = MathTex("d^2_{} = ".format(i+1)).next_to(cum_formula, UP, buff=0).shift(i * 0.65 * UP + 0.2 * UP + 0.40 * RIGHT)
+            new_dist_value2 = MathTex("{:.2f}".format(self.calc_square_dist(projections["points"][i]))).next_to(new_dist_formula2, RIGHT, buff=0).shift(0.10 * RIGHT + 0.05 * DOWN)
+            cum_total += self.calc_square_dist(projections["points"][i])
+            new_cum_value = MathTex("{:.2f}".format(cum_total)).to_corner(DR).shift(0.20 * UP)
+
+            trans_brace = Transform(dist_brace, new_dist_brace, run_time = anim_run_time)
+            trans_formula = Transform(dist_formula, new_dist_formula, run_time = anim_run_time)
+            trans_value = Transform(dist_value, new_dist_value, run_time = anim_run_time)
+            create_formula = Create(new_dist_formula2, run_time = anim_run_time/100)
+            create_value = Create(new_dist_value2, run_time = anim_run_time/100)
+            trans_cum_value = Transform(cum_value, new_cum_value, run_time = anim_run_time)
+
+            self.play(trans_brace, trans_formula, trans_value, create_formula, create_value, trans_cum_value)
+        self.wait(4)
+
+        
 class pc1_find(PCA):
     def construct(self):        
         data = self.import_points()
         points_scaled = VGroup(*[Dot([row["a_scaled"], row["b_scaled"], 0], color = PROMUTUEL_YELLOW) for _, row in data.iterrows()])
 
-        PC1 = Line([-10, -0, 0], [10, 0, 0])
+        PC1 = Line([-10, -0, 0], [10, 0, 0], color = BLUE)
 
         projections = self.create_projections(points_scaled, PC1)
 
@@ -145,11 +206,29 @@ class pc1_find(PCA):
 
         self.initial_setup()       
         self.add(self.plane, PC1, points_scaled, projections, formula, dist_indicator)
-        self.wait(0.5)
         self.play(Rotate(PC1, angle = TAU/2, about_point = ORIGIN, run_time = 10))
-        self.wait(0.5)
+        self.wait(4)
+
+
+class pc1_set(PCA):
+    def construct(self):  
+        data = self.import_points()
+        points_scaled = VGroup(*[Dot([row["a_scaled"], row["b_scaled"], 0], color = PROMUTUEL_YELLOW) for _, row in data.iterrows()])
+
+        PC1 = Line([-10, -0, 0], [10, 0, 0], color = BLUE)
+
+        projections = self.create_projections(points_scaled, PC1)
+
+        formula = MathTex(r"\sum d^2_i = ").to_corner(DR).shift(1.15 * LEFT)
+        dist_indicator = MathTex("{:.2f}".format(self.calc_square_dist(projections["points"]))).to_corner(DR).shift(0.20 * UP)
+        dist_indicator.add_updater(lambda d: d.become(MathTex("{:.2f}".format(self.calc_square_dist(projections["points"]))).to_corner(DR).shift(0.20 * UP)))
+        
+        self.initial_setup()      
+        self.add(self.plane, PC1, points_scaled, projections, formula, dist_indicator)
         self.play(Rotate(PC1, angle = TAU/8, about_point = ORIGIN, run_time = 2))
-        self.wait(1.5)
+        self.wait(0.5)
+        self.play(Transform(PC1, Line([-10, 0, 0], [10, 0, 0], color = PROMUTUEL_YELLOW).rotate(angle = TAU/8, about_point = ORIGIN), run_time = 0.1))
+        self.wait(4)
 
         
 class pc2_find(PCA):
@@ -161,7 +240,7 @@ class rotate_axes(PCA):
         data = self.import_points()
         points_scaled = VGroup(*[Dot([row["a_scaled"], row["b_scaled"], 0], color = PROMUTUEL_YELLOW) for _, row in data.iterrows()])
 
-        PC1 = Line([-10, -0, 0], [10, 0, 0]).rotate(TAU/8, about_point = ORIGIN)
+        PC1 = Line([-10, -0, 0], [10, 0, 0], color = BLUE).rotate(TAU/8, about_point = ORIGIN)
         PC2 = Line([-10, -0, 0], [10, 0, 0]).rotate(-TAU/8, about_point = ORIGIN)
 
         self.initial_setup()
